d4556e220b0b0e4a404dafea0cfc1e94baa781c9
ExlxlxS <denis.bondarenko.pl@gmail.com>
2022-07-26 12:54:37 +0200

    feat: add feature 'increase book amount in cart'

--- pages/main/script.js
+++ pages/main/script.js
@@ -18,14 +18,53 @@ const booksInCart = [{
     "description": "Node.js in Action, Second Edition is a thoroughly revised book based on the best-selling first edition. It starts at square one and guides you through all the features, techniques, and concepts you'll need to build production-quality Node applications."
   }];
 
+const ifBookInCart = function(bookTitle) {
+    for (let cartCard of document.querySelector(".cart-slider").children) {
+        if (bookTitle === cartCard.querySelector('.cart-card-title').innerHTML) {
+            return true;
+        }
+    }
+    return false;
+}
+
+const increaseBookAmount = function(bookTitle) {
+    let currentBookAmountElement;
+    let bookPrice;
+    let currentTotalPriceElement;
+    for (let cartCard of document.querySelector('.cart-slider').children) {
+        if (bookTitle === cartCard.querySelector('.cart-card-title').innerHTML) {
+            currentBookAmountElement = cartCard.querySelector('.cart-card-summary').firstElementChild.firstElementChild;
+            currentBookAmountElement.innerHTML =  Number(currentBookAmountElement.innerHTML) + 1;
+            bookPrice = cartCard.querySelector('.cart-card-summary').firstElementChild.lastElementChild.innerHTML.slice(0,-1);
+            currentTotalPriceElement = document.querySelector('.cart-confirm-button').firstElementChild;
+            currentTotalPriceElement.innerHTML = Number(currentTotalPriceElement.innerHTML.slice(0,-1)) + Number(bookPrice) + '$';
+        }
+    }
+    return false;
+}
+
+const catalogUserInteractive = function(event) {
+    if (event.target.className === 'card-button-add') {
+        const card = event.target.parentElement.parentElement;
+        const title = card.querySelector('.card-title').innerHTML;
+        if (ifBookInCart(title)) {
+            console.log('Yes, it`s in the cart'); //delete this line before release
+            increaseBookAmount(title);
+        } else {
+            console.log('No, there is no book in the cart'); //delete this line before release
+            for (let book of books) {
+                console.log(book);
+            }
+        }
+    }
+}
+
 fetch('../../assets/json/books.json') //path to the file with json data
         .then(response => {
             return response.json();
         })
         .then(books => {
             document.body.prepend(createCatalogePage(books, booksInCart));
-            addEventListener
-            const book = document.querySelector(".card-container");
-            book.addEventListener("click", event => alert(event.currentTarget.className));
+            document.querySelector(".card-container").addEventListener("click", catalogUserInteractive);
         });
 
