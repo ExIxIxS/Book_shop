d7927d482107daffbb0755f18d01d515196266b6
ExlxlxS <denis.bondarenko.pl@gmail.com>
2022-07-29 15:52:25 +0200

    refactor: style format

--- pages/form/script.js
+++ pages/form/script.js
@@ -9,7 +9,14 @@ import {cartUserInteractive
 import {booksInCart
 } from '../../assets/js/commonActions.js';
 
+const formUserInteractive = function(event) {
+        console.log('it works');
+        const formFieldName = querySelector('#name');
+
+}
+
 document.querySelector('.container_centered').prepend(createHeader());
 document.querySelector('main').prepend(createCartSection(booksInCart()));
 document.querySelector('.cart').addEventListener('click', event => cartUserInteractive(event));
 document.querySelector('.container_centered').append(createFooter());
+document.addEventListener('DOMContentLoaded', event => formUserInteractive(event));
