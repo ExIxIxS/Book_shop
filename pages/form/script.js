a0bb8603dbdce76fe34c089b017018398e151bef
ExlxlxS <denis.bondarenko.pl@gmail.com>
2022-07-29 19:10:40 +0200

    feat: add validation for 2 first fields

--- pages/form/script.js
+++ pages/form/script.js
@@ -1,6 +1,7 @@
 import {createHeader,
         createCartSection,
-        createFooter
+        createFooter,
+        createCompleteElement
 } from '../../assets/js/create.js';
 
 import {cartUserInteractive
@@ -9,14 +10,61 @@ import {cartUserInteractive
 import {booksInCart
 } from '../../assets/js/commonActions.js';
 
-const formUserInteractive = function(event) {
-        console.log('it works');
-        const formFieldName = querySelector('#name');
-
+const makeInputInvalid = function(inputElement) {
+    if (inputElement.className !== 'invalid') {
+        const invalidMessageElement = createCompleteElement('span', 'invalid-message', '* The field is invalid');
+        invalidMessageElement.title = 'Please check and enter valid data';
+        inputElement.classList.add('invalid');
+        inputElement.after(invalidMessageElement);
+    }
 }
 
+const makeInputValid = function(inputElement) {
+        if (inputElement.classList.contains('invalid')) {
+            inputElement.classList.remove('invalid');
+            inputElement.nextElementSibling.remove();
+        }
+    }
+
+const checkNameField = function(inputElement) {
+    const NAME = inputElement.value;
+    //NAME (mandatory, the length not less than 4 symbols, strings only)
+    const validString = (NAME.match(/\p{Letter}/gu) ?? []).join('')
+    console.log('validString is "' + validString + '"'); //delete this before release
+    if (validString.length === NAME.length && NAME.length >= 4) {
+        //console.log(validString);
+        makeInputValid(inputElement);
+        //checkFormForValidData();
+    } else {
+        makeInputInvalid(inputElement);
+    }
+}
+
+const checkSurnameField = function(inputElement) {
+    const SURNAME = inputElement.value;
+    //SURNAME (mandatory, the length not less than 5 symbols, strings only)
+    const validString = (SURNAME.match(/\p{Letter}/gu) ?? []).join('')
+    console.log('validString is "' + validString + '"');
+        if (validString.length === SURNAME.length && SURNAME.length >= 5) {
+            //console.log(validString);
+            makeInputValid(inputElement);
+            //checkFormForValidData();
+        } else {
+            makeInputInvalid(inputElement);
+        }
+}
+
+const formUserInteractive = function() {
+    const formNameInput = document.querySelector('#name');
+    formNameInput.addEventListener('blur', event => checkNameField(formNameInput));
+    const formSurnameInput = document.querySelector('#surname');
+    formSurnameInput.addEventListener('blur', event => checkSurnameField(formSurnameInput));
+}
+
+//Creating and adding of header, cart and footer
 document.querySelector('.container_centered').prepend(createHeader());
 document.querySelector('main').prepend(createCartSection(booksInCart()));
-document.querySelector('.cart').addEventListener('click', event => cartUserInteractive(event));
 document.querySelector('.container_centered').append(createFooter());
-document.addEventListener('DOMContentLoaded', event => formUserInteractive(event));
+
+document.querySelector('.cart').addEventListener('click', event => cartUserInteractive(event));
+document.addEventListener('DOMContentLoaded', formUserInteractive);
