676ecdfd54de1e03f69cd45a8f73a86483c1ffd3
unknown <denis.bondarenko.pl@gmail.com>
2022-07-30 20:05:38 +0200

    feat: add date and payment form validation

--- pages/form/script.js
+++ pages/form/script.js
@@ -20,11 +20,15 @@ const makeInputInvalid = function(inputElement) {
 }
 
 const makeInputValid = function(inputElement) {
-        if (inputElement.classList.contains('invalid')) {
-            inputElement.classList.remove('invalid');
-            inputElement.nextElementSibling.remove();
-        }
+    if (inputElement.classList.contains('invalid')) {
+        inputElement.classList.remove('invalid');
+        inputElement.nextElementSibling.remove();
     }
+}
+
+const checkFormForValidData = function() {
+    console.log('Cool!')
+}
 
 const checkNameField = function(inputElement) {
     const NAME = inputElement.value;
@@ -42,12 +46,12 @@ const checkSurnameField = function(inputElement) {
     const SURNAME = inputElement.value;
     //SURNAME (mandatory, the length not less than 5 symbols, strings only)
     const validString = (SURNAME.match(/\p{Letter}{5,}/u) ?? []).join('')
-        if (validString.length === SURNAME.length && SURNAME.length !== 0) {
-            makeInputValid(inputElement);
-            //checkFormForValidData();
-        } else {
-            makeInputInvalid(inputElement);
-        }
+    if (validString.length === SURNAME.length && SURNAME.length !== 0) {
+        makeInputValid(inputElement);
+        //checkFormForValidData();
+    } else {
+         makeInputInvalid(inputElement);
+    }
 }
 
 const checkStreetField = function(inputElement) {
@@ -55,53 +59,75 @@ const checkStreetField = function(inputElement) {
     //STREET mandatory, the length not less than 5 symbols, the numbers are allowed
     //spaces also allowed, if not, delete '\s*'
     const validString = (STREET.match(/(\p{N}*\s*\p{Letter}+\s*\p{N}*)+/u) ?? [[]])
-        if (validString[0].length === STREET.length && validString[0].length >= 5) {
-            makeInputValid(inputElement);
-            //checkFormForValidData();
-        } else {
-            makeInputInvalid(inputElement);
-        }
+    if (validString[0].length === STREET.length && validString[0].length >= 5) {
+        makeInputValid(inputElement);
+        //checkFormForValidData();
+    } else {
+        makeInputInvalid(inputElement);
+    }
 }
 
 const checkHouseNumField = function(inputElement) {
     const HOUSENUMBER = inputElement.value;
     //HOUSENUMBER (mandatory, numbers only, positive numbers only)
     const validString = (HOUSENUMBER.match(/\d+/) ?? [[]])
-        if (validString[0].length === HOUSENUMBER.length  && HOUSENUMBER.length !== 0) {
-            makeInputValid(inputElement);
-            //checkFormForValidData();
-        } else {
-            makeInputInvalid(inputElement);
-        }
+    if (validString[0].length === HOUSENUMBER.length  && HOUSENUMBER.length !== 0) {
+        makeInputValid(inputElement);
+        //checkFormForValidData();
+    } else {
+        makeInputInvalid(inputElement);
+    }
 }
 
 const checkFlatNumField = function(inputElement) {
     const FLATNUMBER = inputElement.value;
     /*
-        FLATNUMBER Flat number(mandatory, numbers only, positive numbers only, the dash symbol is allowed.
+        FLATNUMBER (mandatory, numbers only, positive numbers only, the dash symbol is allowed.
         Means, the flat number shouldn't start with minus/dash symbol.
         For example: -37 is invalid, but 1-37 is valid)
     */
     const validString = (FLATNUMBER.match(/\d(-?\d+)*/) ?? [[]])
-        if (validString[0].length === FLATNUMBER.length  && FLATNUMBER.length !== 0) {
-            makeInputValid(inputElement);
-            //checkFormForValidData();
-        } else {
-            makeInputInvalid(inputElement);
+    if (validString[0].length === FLATNUMBER.length  && FLATNUMBER.length !== 0) {
+        makeInputValid(inputElement);
+        //checkFormForValidData();
+    } else {
+        makeInputInvalid(inputElement);
+    }
+}
+
+const getMinDateString = function() {
+    const minDayObj = new Date(Date.now() + 24 * 36 * 10 ** 5); //now + 24h (*in ms)
+    let months = String(minDayObj.getMonth() + 1); // getMonth() return mounth number [0-11]
+    if (months.length !== 2) {
+        months = '0' + months;
+    }
+    const minDeliveryDate = `${minDayObj.getFullYear()}-${months}-${minDayObj.getDate()}`
+    return minDeliveryDate;
+}
+
+const checkDateInput = function(inputElement) {
+    const USERDATE = inputElement.value;
+    //USERDATE mandatory, not earlier than next day
+    if (Date.parse(USERDATE) >= Date.parse(getMinDateString())) {
+        makeInputValid(inputElement);
+        //checkFormForValidData();
+    } else {
+        makeInputInvalid(inputElement);
+    }
+}
+
+const checkPaymentInput = function(paymentFieldsetElement ) {
+    const inputsCollection = paymentFieldsetElement.querySelectorAll('input');
+    for (let input of inputsCollection) {
+        if (input.checked) {
+            makeInputValid(paymentFieldsetElement);
+            checkFormForValidData();
+            return;
         }
-}
-/*
-const getNextDayString = function() {
-
+    }
+    makeInputInvalid(paymentFieldsetElement);
 }
 
-const setMinDateInput = function() {
-    const todayObj = new Date();
-    const months = String(TODAY.getMonth() + 1);
-
-    const nextDayString = `${TODAY.getFullYear()}-${TODAY.getMonth() + 1}-${TODAY.getDate()}`)
-}
-*/
 const formUserInteractive = function() {
     const formNameInput = document.querySelector('#name');
     formNameInput.addEventListener('blur', event => checkNameField(formNameInput));
@@ -113,13 +139,21 @@ const formUserInteractive = function() {
     formHouseNumInput.addEventListener('blur', event => checkHouseNumField(formHouseNumInput));
     const formFlatNumInput = document.querySelector('#flat-number');
     formFlatNumInput.addEventListener('blur', event => checkFlatNumField(formFlatNumInput));
-    setMinDateInput();
+    const formDeliveryDateInput = document.querySelector('#date-delivery');
+    formDeliveryDateInput.min = getMinDateString();
+    formDeliveryDateInput.addEventListener('blur', event => checkDateInput(formDeliveryDateInput));
+    const formPaymentFieldset = document.querySelector('#payment-method');
+    formPaymentFieldset.addEventListener('change', event => checkPaymentInput(formPaymentFieldset));
+    const formCashRadioInput = document.querySelector('#cash');
+    formCashRadioInput.addEventListener('blur', event => checkPaymentInput(formCashRadioInput.parentElement.parentElement));
+    const formCardRadioInput = document.querySelector('#card');
+    formCardRadioInput.addEventListener('blur', event => checkPaymentInput(formCardRadioInput.parentElement.parentElement));
 }
 
 //Creating and adding of header, cart and footer
 document.querySelector('.container_centered').prepend(createHeader());
 document.querySelector('main').prepend(createCartSection(booksInCart()));
 document.querySelector('.container_centered').append(createFooter());
-
+//User Interactive
 document.querySelector('.cart').addEventListener('click', event => cartUserInteractive(event));
 document.addEventListener('DOMContentLoaded', formUserInteractive);
