aa282dea838c04fdf2ad59d85a0efb5eb37ef0eb
ExlxlxS <denis.bondarenko.pl@gmail.com>
2022-07-30 18:10:29 +0200

    feat: add validation for address fields

--- pages/form/script.js
+++ pages/form/script.js
@@ -29,10 +29,8 @@ const makeInputValid = function(inputElement) {
 const checkNameField = function(inputElement) {
     const NAME = inputElement.value;
     //NAME (mandatory, the length not less than 4 symbols, strings only)
-    const validString = (NAME.match(/\p{Letter}/gu) ?? []).join('')
-    console.log('validString is "' + validString + '"'); //delete this before release
-    if (validString.length === NAME.length && NAME.length >= 4) {
-        //console.log(validString);
+    const validString = (NAME.match(/\p{Letter}{4,}/gu) ?? []).join('')
+    if (validString.length === NAME.length && NAME.length !== 0) {
         makeInputValid(inputElement);
         //checkFormForValidData();
     } else {
@@ -43,10 +41,8 @@ const checkNameField = function(inputElement) {
 const checkSurnameField = function(inputElement) {
     const SURNAME = inputElement.value;
     //SURNAME (mandatory, the length not less than 5 symbols, strings only)
-    const validString = (SURNAME.match(/\p{Letter}/gu) ?? []).join('')
-    console.log('validString is "' + validString + '"');
-        if (validString.length === SURNAME.length && SURNAME.length >= 5) {
-            //console.log(validString);
+    const validString = (SURNAME.match(/\p{Letter}{5,}/u) ?? []).join('')
+        if (validString.length === SURNAME.length && SURNAME.length !== 0) {
             makeInputValid(inputElement);
             //checkFormForValidData();
         } else {
@@ -54,11 +50,70 @@ const checkSurnameField = function(inputElement) {
         }
 }
 
+const checkStreetField = function(inputElement) {
+    const STREET = inputElement.value;
+    //STREET mandatory, the length not less than 5 symbols, the numbers are allowed
+    //spaces also allowed, if not, delete '\s*'
+    const validString = (STREET.match(/(\p{N}*\s*\p{Letter}+\s*\p{N}*)+/u) ?? [[]])
+        if (validString[0].length === STREET.length && validString[0].length >= 5) {
+            makeInputValid(inputElement);
+            //checkFormForValidData();
+        } else {
+            makeInputInvalid(inputElement);
+        }
+}
+
+const checkHouseNumField = function(inputElement) {
+    const HOUSENUMBER = inputElement.value;
+    //HOUSENUMBER (mandatory, numbers only, positive numbers only)
+    const validString = (HOUSENUMBER.match(/\d+/) ?? [[]])
+        if (validString[0].length === HOUSENUMBER.length  && HOUSENUMBER.length !== 0) {
+            makeInputValid(inputElement);
+            //checkFormForValidData();
+        } else {
+            makeInputInvalid(inputElement);
+        }
+}
+
+const checkFlatNumField = function(inputElement) {
+    const FLATNUMBER = inputElement.value;
+    /*
+        FLATNUMBER Flat number(mandatory, numbers only, positive numbers only, the dash symbol is allowed.
+        Means, the flat number shouldn't start with minus/dash symbol.
+        For example: -37 is invalid, but 1-37 is valid)
+    */
+    const validString = (FLATNUMBER.match(/\d(-?\d+)*/) ?? [[]])
+        if (validString[0].length === FLATNUMBER.length  && FLATNUMBER.length !== 0) {
+            makeInputValid(inputElement);
+            //checkFormForValidData();
+        } else {
+            makeInputInvalid(inputElement);
+        }
+}
+/*
+const getNextDayString = function() {
+
+}
+
+const setMinDateInput = function() {
+    const todayObj = new Date();
+    const months = String(TODAY.getMonth() + 1);
+
+    const nextDayString = `${TODAY.getFullYear()}-${TODAY.getMonth() + 1}-${TODAY.getDate()}`)
+}
+*/
 const formUserInteractive = function() {
     const formNameInput = document.querySelector('#name');
     formNameInput.addEventListener('blur', event => checkNameField(formNameInput));
     const formSurnameInput = document.querySelector('#surname');
     formSurnameInput.addEventListener('blur', event => checkSurnameField(formSurnameInput));
+    const formStreetInput = document.querySelector('#street');
+    formStreetInput.addEventListener('blur', event => checkStreetField(formStreetInput));
+    const formHouseNumInput = document.querySelector('#house-number');
+    formHouseNumInput.addEventListener('blur', event => checkHouseNumField(formHouseNumInput));
+    const formFlatNumInput = document.querySelector('#flat-number');
+    formFlatNumInput.addEventListener('blur', event => checkFlatNumField(formFlatNumInput));
+    setMinDateInput();
 }
 
 //Creating and adding of header, cart and footer
